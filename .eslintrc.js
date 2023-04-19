2b8ee9286a689f9068fbde5a65e69b62aa79f166
kuzanatoliy <njkznu@gmail.com>
2023-04-19 22:30:56 +0300

    chore: set up husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    "plugin:solid/recommended",
+    'plugin:solid/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
