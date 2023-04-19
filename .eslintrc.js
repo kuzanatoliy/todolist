2ed5628ff48b7c78aa4e036561c8ef6edc3ed286
kuzanatoliy <njkznu@gmail.com>
2023-04-19 22:12:02 +0300

    chore: add solid eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:solid/recommended",
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
@@ -16,6 +17,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'solid', 'prettier'],
   rules: {},
 };
