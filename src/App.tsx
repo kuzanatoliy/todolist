918a0ced91827e86ed79a4616688975748feceb7
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-06-02 17:50:00 +0300

    fix: remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.scss';
 import { purposeModel } from './models';
 
 const App: Component = () => {
   purposeModel.get().then((res) => console.log(res));
 
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href='https://github.com/solidjs/solid'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  return <div class={styles.App}>test</div>;
 };
 
 export default App;
