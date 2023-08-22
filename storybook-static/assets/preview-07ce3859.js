79aa91d442bcb4550db3ec7722fd74b4ef2078e3
kuzanatoliy <njkznu@gmail.com>
2023-08-22 19:56:33 +0000

    deploy to gh-pages

---
+++ storybook-static/assets/preview-07ce3859.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-d2dd09e2.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-43b9c642.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-43b9c642.js","./iframe-d2dd09e2.js","./_commonjsHelpers-725317a4.js","./index-d475d2ea.js","./index-d37d4223.js","./index-55a653d8.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-07ce3859.js.map
